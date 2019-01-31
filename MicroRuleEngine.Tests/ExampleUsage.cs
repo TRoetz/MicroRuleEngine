06a29a0502b360e9cc92a80c0f1aedc8ed58139b
Jeremy Oursler <jeremy@rubberchickenparadise.com>
2019-01-30 20:10:02 -0600

    Add multiframework targeting
    Added multiframework targeting for .net framework 4, 4.5.2, and 4.6.1 and .net standard 2.0 (could probably target .net standard 1.6 but may not be necessary)
    Set language version for VS2015 (C#6)
    Add methods to get the Expression<Func<T, Bool>>, Primary usecase passing the expression to EF
    Added example EF usage

--- MicroRuleEngine.Tests/ExampleUsage.cs
+++ MicroRuleEngine.Tests/ExampleUsage.cs
@@ -2,242 +2,261 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MicroRuleEngine.Tests.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MicroRuleEngine.Tests
 {
     /// <summary>
-    /// Summary description for UnitTest1
+    ///     Summary description for UnitTest1
     /// </summary>
     [TestClass]
     public class ExampleUsage
     {
         [TestMethod]
-        public void ChildPropertiesOfNull()
+        public void AnyOperator()
         {
-            Order order = GetOrder();
-            order.Customer = null;
-            Rule rule = new Rule
-            {
-                MemberName = "Customer.Country.CountryCode",
-                Operator = ExpressionType.Equal.ToString("g"),
-                TargetValue = "AUS"
-            };
-            MRE engine = new MRE();
-            var compiledRule = engine.CompileRule<Order>(rule);
-            bool passes = compiledRule(order);
-            Assert.IsFalse(passes);
-        }
+            var order = GetOrder();
 
-        [TestMethod]
-        public void ChildProperties()
-        {
-            Order order = GetOrder();
-            Rule rule = new Rule
-            {
-                MemberName = "Customer.Country.CountryCode",
-                Operator = ExpressionType.Equal.ToString("g"),
-                TargetValue = "AUS"
-            };
-            MRE engine = new MRE();
-            var compiledRule = engine.CompileRule<Order>(rule);
-            bool passes = compiledRule(order);
+            //order.Items.Any(a => a.ItemCode == "test")
+            var rule = new Rule
+                       {
+                           MemberName = "Items", // The array property
+                           Operator   = "Any",
+                           Rules = new[]
+                                   {
+                                       new Rule
+                                       {
+                                           MemberName  = "ItemCode", // the property in the above array item
+                                           Operator    = "Equal",
+                                           TargetValue = "Test"
+                                       }
+                                   }
+                       };
+            var engine     = new MRE();
+            var boolMethod = engine.CompileRule<Order>(rule);
+            var passes     = boolMethod(order);
             Assert.IsTrue(passes);
 
-            order.Customer.Country.CountryCode = "USA";
-            passes = compiledRule(order);
-            Assert.IsFalse(passes);
-        }
-
-        [TestMethod]
-
-        public void ConditionalLogic()
-        {
-            Order order = GetOrder();
-            Rule rule = new Rule
-            {
-                Operator = ExpressionType.AndAlso.ToString("g"),
-                                Rules = new List<Rule>
-                            {
-                    new Rule { MemberName = "Customer.LastName", TargetValue = "Doe", Operator = "Equal"},
-                    new Rule
-                {
-                        Operator = "Or",
-                        Rules = new List<Rule>
-                                    {
-                            new Rule { MemberName = "Customer.FirstName", TargetValue = "John", Operator = "Equal"},
-                            new Rule { MemberName = "Customer.FirstName", TargetValue = "Jane", Operator = "Equal"}
-                        }
-                    }
-                }
-            };
-            MRE engine = new MRE();
-            var fakeName = engine.CompileRule<Order>(rule);
-            bool passes = fakeName(order);
-            Assert.IsTrue(passes);
-
-            order.Customer.FirstName = "Philip";
-            passes = fakeName(order);
+            var item = order.Items.First(x => x.ItemCode == "Test");
+            item.ItemCode = "Changed";
+            passes        = boolMethod(order);
             Assert.IsFalse(passes);
         }
 
         [TestMethod]
         public void BooleanMethods()
         {
-            Order order = GetOrder();
-            Rule rule = new Rule
-            {
-                Operator = "HasItem",//The Order Object Contains a method named 'HasItem' that returns true/false
-                Inputs = new List<object> { "Test" }
-            };
-            MRE engine = new MRE();
+            var order = GetOrder();
+            var rule = new Rule
+                       {
+                           Operator = "HasItem", //The Order Object Contains a method named 'HasItem' that returns true/false
+                           Inputs   = new List<object> {"Test"}
+                       };
+            var engine = new MRE();
 
             var boolMethod = engine.CompileRule<Order>(rule);
-            bool passes = boolMethod(order);
+            var passes     = boolMethod(order);
             Assert.IsTrue(passes);
 
             var item = order.Items.First(x => x.ItemCode == "Test");
             item.ItemCode = "Changed";
-            passes = boolMethod(order);
+            passes        = boolMethod(order);
             Assert.IsFalse(passes);
         }
 
         [TestMethod]
         public void BooleanMethods_ByType()
         {
-            Order order = GetOrder();
-            Rule rule = new Rule
-            {
-                Operator = "HasItem",//The Order Object Contains a method named 'HasItem' that returns true/false
-                Inputs = new List<object> { "Test" }
-            };
-            MRE engine = new MRE();
+            var order = GetOrder();
+            var rule = new Rule
+                       {
+                           Operator = "HasItem", //The Order Object Contains a method named 'HasItem' that returns true/false
+                           Inputs   = new List<object> {"Test"}
+                       };
+            var engine = new MRE();
 
-            var boolMethod = engine.CompileRule(typeof(Order), rule);
-            bool passes =(bool)  boolMethod.DynamicInvoke(order);
+            var boolMethod = engine.CompileRule(typeof(Order),
+                                                rule);
+            var passes = (bool) boolMethod.DynamicInvoke(order);
             Assert.IsTrue(passes);
 
             var item = order.Items.First(x => x.ItemCode == "Test");
             item.ItemCode = "Changed";
-            passes = (bool)boolMethod.DynamicInvoke(order);
+            passes        = (bool) boolMethod.DynamicInvoke(order);
             Assert.IsFalse(passes);
         }
-
-         [TestMethod]
-        public void AnyOperator()
-        {
-            Order order = GetOrder();
-            //order.Items.Any(a => a.ItemCode == "test")
-            Rule rule = new Rule
-            {
-                MemberName = "Items",// The array property
-                Operator = "Any",
-                Rules = new[]
-                {
-                     new Rule
-                     {
-                         MemberName = "ItemCode", // the property in the above array item
-                        Operator = "Equal",
-                         TargetValue = "Test",
-                     }
-                 }
-            };
-            MRE engine = new MRE();
-            var boolMethod = engine.CompileRule<Order>(rule);
-            bool passes = boolMethod(order);
-            Assert.IsTrue(passes);
-
-            var item = order.Items.First(x => x.ItemCode == "Test");
-            item.ItemCode = "Changed";
-            passes = boolMethod(order);
-            Assert.IsFalse(passes);
-        }
- 
-
 
         [TestMethod]
-         public void ChildPropertyBooleanMethods()
+        public void ChildProperties()
         {
-            Order order = GetOrder();
-            Rule rule = new Rule
-            { 
-                MemberName = "Customer.FirstName",
-                Operator = "EndsWith",//Regular method that exists on string.. As a note expression methods are not available
-                Inputs = new List<object> { "ohn" }
-            };
-            MRE engine = new MRE();
+            var order = GetOrder();
+            var rule = new Rule
+                       {
+                           MemberName  = "Customer.Country.CountryCode",
+                           Operator    = ExpressionType.Equal.ToString("g"),
+                           TargetValue = "AUS"
+                       };
+            var engine       = new MRE();
+            var compiledRule = engine.CompileRule<Order>(rule);
+            var passes       = compiledRule(order);
+            Assert.IsTrue(passes);
+
+            order.Customer.Country.CountryCode = "USA";
+            passes                             = compiledRule(order);
+            Assert.IsFalse(passes);
+        }
+
+        /// <summary>
+        ///     With out error handling this throws an exception.
+        ///     With error handling EF usage fails
+        /// </summary>
+        [TestMethod]
+        public void ChildPropertiesOfNull()
+        {
+            var order = GetOrder();
+            order.Customer = null;
+            var rule = new Rule
+                       {
+                           MemberName  = "Customer.Country.CountryCode",
+                           Operator    = ExpressionType.Equal.ToString("g"),
+                           TargetValue = "AUS"
+                       };
+            var engine       = new MRE();
+            var compiledRule = engine.CompileRule<Order>(rule);
+            var passes       = compiledRule(order);
+            Assert.IsFalse(passes);
+        }
+
+        [TestMethod]
+        public void ChildPropertyBooleanMethods()
+        {
+            var order = GetOrder();
+            var rule = new Rule
+                       {
+                           MemberName = "Customer.FirstName",
+                           Operator =
+                               "EndsWith", //Regular method that exists on string.. As a note expression methods are not available
+                           Inputs = new List<object> {"ohn"}
+                       };
+            var engine         = new MRE();
             var childPropCheck = engine.CompileRule<Order>(rule);
-            bool passes = childPropCheck(order);
+            var passes         = childPropCheck(order);
             Assert.IsTrue(passes);
 
             order.Customer.FirstName = "jane";
-            passes = childPropCheck(order);
+            passes                   = childPropCheck(order);
+            Assert.IsFalse(passes);
+        }
+
+        /// <summary>
+        ///     With out error handling this throws an exception.
+        ///     With error handling EF usage fails
+        /// </summary>
+        [TestMethod]
+        public void ChildPropertyOfNullBooleanMethods()
+        {
+            var order = GetOrder();
+            order.Customer = null;
+            var rule = new Rule
+                       {
+                           MemberName = "Customer.FirstName",
+                           Operator =
+                               "EndsWith", //Regular method that exists on string.. As a note expression methods are not available
+                           Inputs = new List<object> {"ohn"}
+                       };
+            var engine         = new MRE();
+            var childPropCheck = engine.CompileRule<Order>(rule);
+            var passes         = childPropCheck(order);
             Assert.IsFalse(passes);
         }
 
         [TestMethod]
-         public void ChildPropertyOfNullBooleanMethods()
-         {
-             Order order = GetOrder();
-             order.Customer = null;
-             Rule rule = new Rule
-             {
-                 MemberName = "Customer.FirstName",
-                 Operator = "EndsWith", //Regular method that exists on string.. As a note expression methods are not available
-                 Inputs = new List<object> { "ohn" }
-             };
-            MRE engine = new MRE();
-            var childPropCheck = engine.CompileRule<Order>(rule);
-             bool passes = childPropCheck(order);
-             Assert.IsFalse(passes);
-         }
-        [TestMethod]
-        public void RegexIsMatch()//Had to add a Regex evaluator to make it feel 'Complete'
+        public void ConditionalLogic()
         {
-            Order order = GetOrder();
-            Rule rule = new Rule
-            {
-                MemberName = "Customer.FirstName",
-                Operator = "IsMatch",
-                TargetValue = @"^[a-zA-Z0-9]*$"
-            };
-            MRE engine = new MRE();
-            var regexCheck = engine.CompileRule<Order>(rule);
-            bool passes = regexCheck(order);
+            var order = GetOrder();
+            var rule = new Rule
+                       {
+                           Operator = ExpressionType.AndAlso.ToString("g"),
+                           Rules = new List<Rule>
+                                   {
+                                       new Rule {MemberName = "Customer.LastName", TargetValue = "Doe", Operator = "Equal"},
+                                       new Rule
+                                       {
+                                           Operator = "Or",
+                                           Rules = new List<Rule>
+                                                   {
+                                                       new Rule
+                                                       {
+                                                           MemberName = "Customer.FirstName", TargetValue = "John",
+                                                           Operator   = "Equal"
+                                                       },
+                                                       new Rule
+                                                       {
+                                                           MemberName = "Customer.FirstName", TargetValue = "Jane",
+                                                           Operator   = "Equal"
+                                                       }
+                                                   }
+                                       }
+                                   }
+                       };
+            var engine   = new MRE();
+            var fakeName = engine.CompileRule<Order>(rule);
+            var passes   = fakeName(order);
             Assert.IsTrue(passes);
 
-            order.Customer.FirstName = "--NoName";
-            passes = regexCheck(order);
+            order.Customer.FirstName = "Philip";
+            passes                   = fakeName(order);
             Assert.IsFalse(passes);
         }
 
         public static Order GetOrder()
         {
-            Order order = new Order()
-            {
-                OrderId = 1,
-                Customer = new Customer()
-                {
-                    FirstName = "John",
-                    LastName = "Doe",
-                    Country = new Country()
-                    {
-                        CountryCode = "AUS"
-                    }
-                },
-                Items = new List<Item>()
-                {
-                    new Item { ItemCode = "MM23", Cost=5.25M},
-                    new Item { ItemCode = "LD45", Cost=5.25M},
-                    new Item { ItemCode = "Test", Cost=3.33M},
-                },
-                Total = 13.83m,
-                OrderDate = new DateTime(1776, 7, 4),
-                Status = Status.Open
-
-            };
+            var order = new Order
+                        {
+                            OrderId = 1,
+                            Customer = new Customer
+                                       {
+                                           FirstName = "John",
+                                           LastName  = "Doe",
+                                           Country = new Country
+                                                     {
+                                                         CountryCode = "AUS"
+                                                     }
+                                       },
+                            Items = new List<Item>
+                                    {
+                                        new Item {ItemCode = "MM23", Cost = 5.25M},
+                                        new Item {ItemCode = "LD45", Cost = 5.25M},
+                                        new Item {ItemCode = "Test", Cost = 3.33M}
+                                    },
+                            Total = 13.83m,
+                            OrderDate = new DateTime(1776,
+                                                     7,
+                                                     4),
+                            Status = Status.Open
+                        };
             return order;
+        }
+
+        [TestMethod]
+        public void RegexIsMatch() //Had to add a Regex evaluator to make it feel 'Complete'
+        {
+            var order = GetOrder();
+            var rule = new Rule
+                       {
+                           MemberName  = "Customer.FirstName",
+                           Operator    = "IsMatch",
+                           TargetValue = @"^[a-zA-Z0-9]*$"
+                       };
+            var engine     = new MRE();
+            var regexCheck = engine.CompileRule<Order>(rule);
+            var passes     = regexCheck(order);
+            Assert.IsTrue(passes);
+
+            order.Customer.FirstName = "--NoName";
+            passes                   = regexCheck(order);
+            Assert.IsFalse(passes);
         }
     }
 }
